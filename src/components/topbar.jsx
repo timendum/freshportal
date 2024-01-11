236e94de26e2d06353f4d9d8f5d8e498baaff48b
Timendum <timedum@gmail.com>
2024-01-11 16:52:00 +0000

    Update topbar.jsx

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -26,7 +26,14 @@ function Button({ onClick, icon, text, tooltip }) {
 export default function Topbar({ handleLogin, isLoggedIn, setAddWiget, setExpImp, toggleDark }) {
   return (
     <div className="flex bg-slate-700 px-2 py-1 text-white shadow-sm dark:text-gray-200 dark:shadow-slate-700 lg:px-4 lg:py-2">
-      <h1 className="grow align-text-bottom text-xl">FreshRSS Portal</h1>
+      <h1 className="grow align-text-bottom text-xl">
+      {!!isLoggedIn && (
+        <a href={freshRss.base.substring(0, freshRss.base.length - 16)} title="Open FreshRSS">FreshRSS Portal</a>
+        )}
+      {!isLoggedIn && (
+        <div>FreshRSS Portal</div>
+        )}
+      </h1>
       <button
         type="button"
         title="Toggle Dark mode"
