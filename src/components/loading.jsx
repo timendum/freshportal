7c9c85f5b017fbd6009c0ba8741b791e478b2965
Timendum <timedum@gmail.com>
2024-06-24 09:42:24 +0000

    Center loading

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -4,7 +4,7 @@ import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 export default function Loading() {
   return (
-    <p className="mx-auto w-1/4 py-1 dark:text-zinc-300">
+    <p className="mx-auto w-1/4 py-1 text-center dark:text-zinc-300">
       <FontAwesomeIcon className="animate-spin" icon={faSpinner} />
       &nbsp; Loading...
     </p>
